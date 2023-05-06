c318648da89b801b25c244b2eeeb1f499640b589
Miguel de Domingo <migueldedomingo12@gmail.com>
2023-05-06 14:46:22 +0200

    Initial commit - empty spring boot proyect

---
+++ src/test/java/com/johndeere/tracking/TmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johndeere.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
